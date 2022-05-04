13c93463e2c10fcdec0c7385e98de8b2e0729274
Aeluxx <matthewgregory@me.com>
2022-05-04 05:38:25 -0400

    Update direct_method.py
    Adjustments and optimization
    
    Also moved main method to new file

--- direct_method.py
+++ direct_method.py
@@ -5,26 +5,19 @@ import pulp
 import numpy as np
 import cplex
 
-
-def directMethod(data, flag, A0, constraints, count_constraint):
-    # Initialising the problem
+def directMethod(ilp, data, query, offsets=None):
+    flag, A0, constraints, count_constraint = query
     if flag:
-        # Maximise
-        problem = pulp.LpProblem("PAQL Maximise", pulp.LpMaximize)
+        problem = pulp.LpProblem("PAQL_Maximise", pulp.LpMaximize)
     else:
-        problem = pulp.LpProblem("PAQL Minimize", pulp.LpMinimize)
+        problem = pulp.LpProblem("PAQL_Minimize", pulp.LpMinimize)
 
-    # Since variables are binary objects (0 or 1)
     indices = LpVariable.dict('x_', data.index, cat='Binary')
-    # print(indices)
 
     variables = indices.values()
-    attr_col = data[A0]
-    # print(attr_col) # dim : n, 1
-    # print(variables) # dim : 1, n
     if A0 is not None:
+        attr_col = data[A0]
         problem += pulp.lpSum(np.transpose(np.asarray(attr_col)) * np.asarray(list(variables)))
-    # print(problem)
     else:
         problem += pulp.lpSum(np.asarray(list(variables)))
 
@@ -36,50 +29,36 @@ def directMethod(data, flag, A0, constraints, count_constraint):
         problem += pulp.lpSum(variables) >= Lc
     if Uc is not None:
         problem += pulp.lpSum(variables) <= Uc
-    # print(problem)
 
     # ATTRIBUTE CONSTRAINT
-    for constraint in constraints:
+    for i in range(0, len(constraints)):
+        offset = 0
+
         attr_variables = []
-        if constraint is not None:
-            attr_variables = np.transpose(np.asarray(data[constraint[0]])) * np.asarray(list(variables))
+
+        if constraints[i] is not None:
+            attr_variables = np.transpose(np.asarray(data[constraints[i][0]])) * np.asarray(list(variables))
+            if offsets is not None:
+                offset = offsets[i]
 
         # Lower bound on attribute
-        if constraint[1] is not None:
-            problem += pulp.lpSum(attr_variables) >= constraint[1]
+        if constraints[i][1] is not None:
+            problem += pulp.lpSum(attr_variables) >= constraints[i][1] + offset
 
         # Upper bound on attribute
-        if constraint[2] is not None:
-            problem += pulp.lpSum(attr_variables) <= constraint[2]
+        if constraints[i][2] is not None:
+            problem += pulp.lpSum(attr_variables) <= constraints[i][2] + offset
 
-    print("Direct")
     # Added the solver
-    ilp = pulp.CPLEX_CMD(path='/Users/jahnavitirunagari/Applications/CPLEX_Studio221/cplex/bin/x86-64_osx/cplex')
     problem.solve(ilp)
-    print("Status:", pulp.LpStatus[problem.status])
+
     # Get the result tuples
-    # print(problem)
     res = []
-    # print(problem.variables())
     for var in problem.variables():
         if var.varValue != 0:
-            # print(var)
-            res.append(data.iloc[int(var.name[3:]), :])
+            index = int(var.name[(var.name.rindex('_') + 1):])
+            res.append(data.loc[index])
 
     # Return the result dataframe
     final_df = pd.DataFrame(res)
     return final_df
-
-
-# Testing
-if __name__ == "__main__":
-    df = pd.read_csv('tpch.csv', sep=',')
-    size = math.ceil(len(df) * .1)
-    small_df = df.head(size)
-
-    print(directMethod(small_df, True, "count_order",
-                       [['sum_base_price', None, 15469853.7043], ['sum_disc_price', None, 45279795.0584],
-                        ['sum_charge', None, 95250227.7918], ['avg_qty', None, 50.353948653],
-                        ['avg_price', None, 68677.5852459], ['avg_disc', None, 0.110243522496],
-                        ['sum_qty', None, 77782.028739]],
-                       {'LC': 1, "UC": None}))
